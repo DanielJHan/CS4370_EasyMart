8064df51b76d32d83ab41131fc38a48d22e5d2df
thinvanadium903 <ppb93083@uga.edu>
2025-05-04 14:02:01 -0400

    installed typescript dependencies, have a semi-working app

--- backend/src/main/java/com/example/demo/Controllers/UserController.java
+++ backend/src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f74f4ddd2354f4c68efe17adeb378ccbe628ea8e36d02b2fc875247d2e11fdab
-size 9478
+oid sha256:fbc2773fc7c0a78671ef3b086ef37d57739da5f647cf685ea1581170f8f01452
+size 9442
--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("18071f232dd156f4")
+/******/ 		__webpack_require__.h = () => ("66ff57e629f3add0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
